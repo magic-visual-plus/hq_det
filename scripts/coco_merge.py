621131906eaf4d2e6ad749bc75dd27be162f003c
zouxiaochuan <zouxiaochuan@163.com>
2026-01-04 16:40:31 +0800

    update code

--- scripts/coco_merge.py
+++ scripts/coco_merge.py
@@ -2,13 +2,21 @@ import sys
 import os
 import json
 import shutil
+import argparse
 
 if __name__ == '__main__':
-    input_paths = sys.argv[1:-1]
-    output_path = sys.argv[-1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_paths', type=str, nargs='+', required=True,
+                        help='List of input dataset paths to merge')
+    parser.add_argument('--output_path', type=str, required=True,
+                        help='Output dataset path')
+    parser.add_argument('--same_category', action='store_true', default=False,
+                        help='Whether to treat categories with the same name as the same category')
+    args = parser.parse_args()
 
+    output_path = args.output_path
     annotations_merged = None
-    for input_path in input_paths:
+    for input_path in args.input_paths:
         with open(os.path.join(input_path, '_annotations.coco.json'), 'r') as f:
             annotations = json.load(f)
             pass
@@ -28,20 +36,34 @@ if __name__ == '__main__':
             image_id_map = dict()
             category_id_map = dict()
             category_names = {cat['name']: cat for cat in annotations_merged['categories']}
+            max_category_id = max(cat['id'] for cat in annotations_merged['categories'])
+            max_image_id = max(img['id'] for img in annotations_merged['images'])
+
             for cat in annotations['categories']:
                 if cat['name'] in category_names:
                     category_id_map[cat['id']] = category_names[cat['name']]['id']
                     pass
                 else:
-                    new_cat = cat.copy()
-                    new_cat['id'] = len(annotations_merged['categories'])
-                    category_id_map[cat['id']] = new_cat['id']
-                    annotations_merged['categories'].append(new_cat)
+                    # add new category
+                    if not args.same_category:
+                        new_cat = cat.copy()
+                        new_cat['id'] = max_category_id + 1
+                        max_category_id += 1
+                        category_id_map[cat['id']] = new_cat['id']
+                        annotations_merged['categories'].append(new_cat)
+                        pass
                     pass
                 pass
 
+            need_category_ids = set(category_id_map.keys())
+            need_image_ids = set(ann['image_id'] for ann in annotations['annotations'] if ann['category_id'] in need_category_ids)
+
             for image in annotations['images']:
-                new_image_id = len(annotations_merged['images'])
+                if image['id'] not in need_image_ids:
+                    continue
+                    pass
+                new_image_id = max_image_id + 1
+                max_image_id += 1
                 image_id_map[image['id']] = new_image_id
                 image['id'] = new_image_id
                 annotations_merged['images'].append(image)
@@ -54,6 +76,9 @@ if __name__ == '__main__':
                 pass
 
             for ann in annotations['annotations']:
+                if ann['category_id'] not in need_category_ids:
+                    continue
+                    pass
                 ann['category_id'] = category_id_map[ann['category_id']]
                 ann['image_id'] = image_id_map[ann['image_id']]
                 ann['id'] = len(annotations_merged['annotations'])
